6cac8ed985c11e24333372d9220b9ffab6d7e4b3
Yonghao Jin <yonghao.jyh1@gmail.com>
2019-08-03 00:21:58 -0400

    sticky buttons in tool panel

--- src/components/Tool/ToolPanel.tsx
+++ src/components/Tool/ToolPanel.tsx
@@ -1,7 +1,7 @@
 import {taskTag, TaskElement, TaskId, TaskListElement, TaskDragType} from "../Types"
 import * as React from "react"
 import * as _ from "lodash"
-import {Header, Button, Segment, Icon} from 'semantic-ui-react'
+import {Header, Button, Segment, Icon, Sticky, Ref} from 'semantic-ui-react'
 import {taskListReq} from "../MockRequests"
 // import {taskListReq} from "../Requests"
 
@@ -12,9 +12,11 @@ import {Path} from './Types'
 
 type ToolPanelState = {tasks: TaskListElement[], loading: boolean, current?: Path}
 export class ToolPanel extends React.Component<{}, ToolPanelState>{
+    contextRef: React.Ref<any>
     constructor(props: {}){
         super(props)
         this.state = {tasks: [], loading: true, current: []}
+        this.contextRef = React.createRef()
     }
 
     cd = (dir: Path) => {
@@ -29,40 +31,42 @@ export class ToolPanel extends React.Component<{}, ToolPanelState>{
 
     render(){
         const {current} = this.state
-
         return(
             <React.Fragment>
-                <Header color='blue' as="h2" attached='top'>Tools</Header>
-                <Segment attached loading={this.state.loading} className="toolpanel">
-                    <Button.Group floated="right" size="small" basic color='blue'>
-                        <Button icon>
-                            <Icon name='add' />
-                            New Folder
-                        </Button>
-                        <Button icon>
-                            <Icon name='add square' />
-                            New File
-                        </Button>
-                        <Button icon>
-                            <Icon name='trash alternate outline' />
-                            Delete
-                        </Button>
-                    </Button.Group>
-
-                    <br style={{clear: "both"}} />
-
-                    <ToolPath 
-                        current={current}
-                        cd={this.cd}
-                    />
-                    
-                    <ToolList
-                        current={current}
-                        cd={this.cd}
-                        tasks={this.state.tasks}
-                    />
-                    
-                </Segment>
+                <Header color='blue' as="h2" attached='top'>Tools</Header> 
+                <Ref innerRef={this.contextRef}>
+                    <Segment attached loading={this.state.loading} className="toolpanel">
+                        <Sticky context={this.contextRef}>
+                            <div className="panelsticky">
+                            <Button.Group floated="right" size="small" basic color='blue'>
+                                <Button icon>
+                                    <Icon name='add' />
+                                    New Folder
+                                </Button>
+                                <Button icon>
+                                    <Icon name='add square' />
+                                    New File
+                                </Button>
+                                <Button icon>
+                                    <Icon name='trash alternate outline' />
+                                    Delete
+                                </Button>
+                            </Button.Group>
+                            <br style={{clear: "both"}} />
+                            <ToolPath 
+                            current={current}
+                            cd={this.cd}
+                            />
+                            </div>
+                        </Sticky>                                       
+                        <ToolList
+                            current={current}
+                            cd={this.cd}
+                            tasks={this.state.tasks}
+                        />
+                
+                    </Segment> 
+                </Ref>                   
             </React.Fragment>
         )
     }
