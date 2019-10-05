038f6a4956649051b8f70c26bc83861a0dc454c7
Yonghao Jin <yonghao.jyh1@gmail.com>
2019-10-04 23:03:43 -0400

    use dompurify to sanitize generated markdown

--- src/components/Worksheet/Markup.tsx
+++ src/components/Worksheet/Markup.tsx
@@ -1,8 +1,10 @@
 import * as marked from 'marked';
 import * as React from 'react'
 
+import * as DOMPurify from 'dompurify'
+
 export const MarkupText: React.SFC<{text: string}> = (props) => {
-    let content = marked(props.text, {sanitize: true})
+    let content = DOMPurify.sanitize(marked(props.text))
     return (
         <div className={"ws-item"}>
             <div dangerouslySetInnerHTML={{ __html: content }} />
