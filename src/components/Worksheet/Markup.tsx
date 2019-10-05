23798bc2c30d2ecc1077dc90a83c96d256cf6656
Yonghao Jin <yonghao.jyh1@gmail.com>
2019-10-04 21:42:20 -0400

    santize html in worksheet markdown

--- src/components/Worksheet/Markup.tsx
+++ src/components/Worksheet/Markup.tsx
@@ -2,7 +2,7 @@ import * as marked from 'marked';
 import * as React from 'react'
 
 export const MarkupText: React.SFC<{text: string}> = (props) => {
-    let content = marked(props.text)
+    let content = marked(props.text, {sanitize: true})
     return (
         <div className={"ws-item"}>
             <div dangerouslySetInnerHTML={{ __html: content }} />
